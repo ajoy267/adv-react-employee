4fe4e1dc168651e789797dcca7dd09493801fed5
ajoy267 <andrew.joy12@hotmail.com>
2022-02-24 12:52:51 -0800

    got my login/register page to render the correct info and work as they should

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
+import AuthButton from '../AuthButton/AuthButton';
 
 export default function Header() {
   const { user } = useUser();
@@ -10,9 +11,7 @@ export default function Header() {
         <h2>ACME Employee Directory</h2>
       </Link>
       <p>{user?.email ? 'Signed in' : 'Not Signed In'}</p>
-      <Link to="/login">
-        <button>Sign In</button>
-      </Link>
+      <AuthButton />
     </header>
   );
 }
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,5 +1,44 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import SignInForm from '../../components/SignInForm/SignInForm';
+import { useUser } from '../../context/UserContext';
+import { signInUser, signUpUser } from '../../services/user';
 
-export default function Auth() {
-  return <div>Auth</div>;
+export default function Auth({ isSigningUp = false }) {
+  const history = useHistory();
+  const { setUser } = useUser();
+
+  const handleAuth = async (email, password) => {
+    try {
+      if (isSigningUp) {
+        await signUpUser(email, password);
+        history.push('/confirm-email');
+      } else {
+        const loggedIn = await signInUser(email, password);
+        setUser({ id: loggedIn.id, email: loggedIn.email });
+        history.replace('/profile');
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
+  return (
+    <section>
+      <h2>{isSigningUp ? 'Welcome!' : 'Welcome Back!'}</h2>
+      <br />
+      <SignInForm
+        onSubmit={handleAuth}
+        label={isSigningUp ? 'Sign Up' : 'Sign In'}
+      />
+      {isSigningUp ? (
+        <p>
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      ) : (
+        <p>
+          Need an account? <Link to="/register">Sign Up</Link>
+        </p>
+      )}
+    </section>
+  );
 }
